2bd2b4ada68f5589392518695c5f11d0b369b807
Sukma Suryaningsih <ingsih19@gmail.com>
2022-06-19 18:33:37 +0800

    menampilkan contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
+import './Contact.css';
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({photo, name, phone, email}) => {
     return (
-        <></>
+        <div className='contact'>
+            <div className='image'>
+                <img src={photo} alt="kucing" />
+            </div>
+            <div className='data'>
+                <h3>{name}</h3>
+                <p>{phone}</p>
+                <p>{email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
+import './Header.css';
 import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h1>Call a Friend</h1>
+            <p>your friendly contact app</p>
+            <div className='dash'></div>
+        </div>
     )
 }
 
